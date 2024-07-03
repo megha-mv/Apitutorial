c4460674587209ec597f8b5771ec1205c6723b06
Megha <megha@nagarro.com>
2024-07-03 16:27:39 +0530

    api creation

---
+++ apitutorial/src/BackEnd/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const users = require('./MOCK_DATA.json');
+
+//setup server
+const app = express();
+const PORT=8000;
+
+//Routes
+
+//html route
+app.get('/users',(req,res)=>{
+    const html =`
+    <ul>
+        ${users.map(user =>`<li>${user.first_name}</li>`).join("")}
+    </ul>
+    `;
+    res.send(html);
+})
+//
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+})
+//
+app.get('/api/users/:id',(req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user)
+})
+
+//
+app.listen(PORT,()=>console.log(`Server listening on PORT:${PORT}`));
